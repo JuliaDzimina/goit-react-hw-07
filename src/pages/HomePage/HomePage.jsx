f1294a7288a20387a9db3e100d88c19246d4db88
JuliaDzimina <132845748+JuliaDzimina@users.noreply.github.com>
2024-08-12 00:20:45 +0300

    ref

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to Contacts App!</h1>
+      <p>Here you can manage your contacts</p>
+    </div>
+  );
+};
+
+export default HomePage;
